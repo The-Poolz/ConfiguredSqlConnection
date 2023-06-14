8ec5b5bae01ee6a9929f1c0e79b8efa6fd2a9a26
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-06-14 17:58:51 +0300

    Add `DbContextConnectionStringFactory` (#11)
    * add DbContextConnectionStringFactory
    
    * mark DbContextConnectionStringFactory as static
    
    * update DbContextOptionsBuilderFactory
    
    * add ConnectionStringFromEnvironmentFactory
    
    * update method names

---
+++ ConfiguredSqlConnection/Extensions/ConnectionStringFactory.cs
@@ -0,0 +1,42 @@
+﻿using SecretsManager;
+using EnvironmentManager;
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfiguredSqlConnection.Extensions;
+
+public static class ConnectionStringFactory
+{
+    public static string GetConnection(ContextOption option, string? dbName = null) =>
+        option switch
+        {
+            ContextOption.Prod => GetConnectionFromSecret(),
+            ContextOption.Staging => GetConnectionFromConfiguration(dbName),
+            _ => throw new InvalidEnumArgumentException(nameof(option), (int)option, typeof(ContextOption)),
+        };
+
+    public static string GetConnectionFromSecret()
+    {
+        var secretValue = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_SECRET_NAME_OF_CONNECTION", true);
+
+        return new SecretManager().GetSecretValue(secretValue, "connectionString");
+    }
+
+    public static string GetConnectionFromConfiguration(string? dbName)
+    {
+        if (string.IsNullOrWhiteSpace(dbName))
+            throw new ArgumentNullException(nameof(dbName));
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Environment.CurrentDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = config.GetConnectionString(dbName);
+        if (string.IsNullOrWhiteSpace(connectionString))
+            throw new InvalidOperationException($"Connection string for database '{dbName}' not found in configuration.");
+
+        return connectionString;
+    }
+}
---
+++ ConfiguredSqlConnection/Extensions/ConnectionStringFromEnvironmentFactory.cs
@@ -0,0 +1,21 @@
+﻿using EnvironmentManager;
+
+namespace ConfiguredSqlConnection.Extensions;
+
+public static class ConnectionStringFromEnvironmentFactory
+{
+    private static string DbMode =>
+        EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_DB_MODE", true);
+    private static string DbName =>
+        EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_DB_NAME");
+
+    public static string GetConnectionFromEnvironment()
+    {
+        if (!Enum.TryParse(typeof(ContextOption), DbMode, true, out var optionObj) || optionObj is not ContextOption option)
+        {
+            throw new ArgumentException($"Invalid value for environment variable 'CONFIGUREDSQLCONNECTION_DB_MODE': {DbMode}");
+        }
+
+        return ConnectionStringFactory.GetConnection(option, DbName);
+    }
+}
