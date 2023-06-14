c8e58b73f8e0ea6113559fd500ed389f72ed4b80
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-14 15:37:54 +0300

    add ConfigurerManager

--- ConfiguredSqlConnection/DataBaseContext.cs
+++ ConfiguredSqlConnection/DataBaseContext.cs
@@ -9,20 +9,5 @@ public class DataBaseContext : DbContext, IAsyncDisposable, IDisposable
     public DataBaseContext() { }
     public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var actionConnection = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_ACTION_CONNECTION");
-        if (!string.IsNullOrEmpty(actionConnection))
-        {
-            optionsBuilder.UseSqlServer(actionConnection);
-        }
-        if (!optionsBuilder.IsConfigured)
-        {
-            var secretValue = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_SECRET_NAME_OF_CONNECTION", true);
-
-            var connectionString = new SecretManager().GetSecretValue(secretValue, "connectionString");
-
-            optionsBuilder.UseSqlServer(connectionString);
-        }
-    }
+    
 }
---
+++ ConfiguredSqlConnection/Extensions/ConfigurerManager.cs
@@ -0,0 +1,26 @@
+﻿using SecretsManager;
+using EnvironmentManager;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConfiguredSqlConnection.Extensions;
+
+public static class ConfigurerManager
+{
+    public static void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var actionConnection = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_ACTION_CONNECTION");
+            if (!string.IsNullOrEmpty(actionConnection))
+            {
+                optionsBuilder.UseSqlServer(actionConnection);
+            }
+
+            var secretValue = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_SECRET_NAME_OF_CONNECTION", true);
+
+            var connectionString = new SecretManager().GetSecretValue(secretValue, "connectionString");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
