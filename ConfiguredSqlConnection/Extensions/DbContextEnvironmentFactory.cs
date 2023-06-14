404093e4a2d25f2a8cb7bcd8a82c302e866dfa40
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-14 16:51:42 +0300

    fix factories, fix existing tests

--- ConfiguredSqlConnection/Extensions/DbContextEnvironmentFactory.cs
+++ ConfiguredSqlConnection/Extensions/DbContextEnvironmentFactory.cs
@@ -1,24 +1,25 @@
 ﻿using EnvironmentManager;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConfiguredSqlConnection.Extensions;
 
-public class DbContextEnvironmentFactory : DbContextFactory
+public class DbContextEnvironmentFactory<TContext> : DbContextFactory<TContext> where TContext : DbContext
 {
     private readonly string dbMode;
     private readonly string dbName;
 
     public DbContextEnvironmentFactory()
-        : this(new DbContextOptionsBuilderFactory<DataBaseContext>())
+        : this(new DbContextOptionsBuilderFactory<TContext>())
     { }
 
-    public DbContextEnvironmentFactory(DbContextOptionsBuilderFactory<DataBaseContext> optionsBuilderFactory)
+    public DbContextEnvironmentFactory(DbContextOptionsBuilderFactory<TContext> optionsBuilderFactory)
         : base(optionsBuilderFactory)
     {
         dbMode = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_DB_MODE", true);
         dbName = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_DB_NAME");
     }
 
-    public virtual DataBaseContext CreateFromEnvironment()
+    public virtual TContext CreateFromEnvironment()
     {
         if (!Enum.TryParse(typeof(ContextOption), dbMode, true, out var optionObj) || optionObj is not ContextOption option)
         {
