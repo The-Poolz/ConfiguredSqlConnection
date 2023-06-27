cf2870b0884884389b0be3c7da766aa3b61091f0
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-27 20:21:43 +0300

    refactor

--- ConfiguredSqlConnection/Extensions/DbContextOptionsBuilderExtensions.cs
+++ ConfiguredSqlConnection/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -6,20 +6,27 @@ namespace ConfiguredSqlConnection.Extensions;
 
 public static class DbContextOptionsBuilderExtensions
 {
-    public static DbContextOptionsBuilder OnConfiguring(this DbContextOptionsBuilder optionsBuilder)
+    public static DbContextOptionsBuilder ConfigureFromActionConnection(this DbContextOptionsBuilder optionsBuilder)
     {
-        if (!optionsBuilder.IsConfigured)
+        if (optionsBuilder.IsConfigured) return optionsBuilder;
+
+        var connectionString = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_ACTION_CONNECTION");
+        if (!string.IsNullOrEmpty(connectionString))
         {
-            var actionConnection = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_ACTION_CONNECTION");
-            if (!string.IsNullOrEmpty(actionConnection))
-            {
-                optionsBuilder.UseSqlServer(actionConnection);
-                return optionsBuilder;
-            }
+            optionsBuilder.UseSqlServer(connectionString);
+        }
 
-            var secretValue = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_SECRET_NAME_OF_CONNECTION", true);
-            var connectionString = new SecretManager().GetSecretValue(secretValue, "connectionString");
+        return optionsBuilder;
+    }
 
+    public static DbContextOptionsBuilder ConfigureFromSecretConnection(this DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured) return optionsBuilder;
+
+        var secretValue = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_SECRET_NAME_OF_CONNECTION", true);
+        var connectionString = new SecretManager().GetSecretValue(secretValue, "connectionString");
+        if (!string.IsNullOrEmpty(connectionString))
+        {
             optionsBuilder.UseSqlServer(connectionString);
         }
 
