1683a26833734dcbeb86963b1ce9729c529a687a
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-27 20:16:32 +0300

    add DbContextOptionsBuilderExtensions

---
+++ ConfiguredSqlConnection/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using SecretsManager;
+using EnvironmentManager;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConfiguredSqlConnection.Extensions;
+
+public static class DbContextOptionsBuilderExtensions
+{
+    public static DbContextOptionsBuilder OnConfiguring(this DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var actionConnection = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_ACTION_CONNECTION");
+            if (!string.IsNullOrEmpty(actionConnection))
+            {
+                optionsBuilder.UseSqlServer(actionConnection);
+                return optionsBuilder;
+            }
+
+            var secretValue = EnvManager.GetEnvironmentValue<string>("CONFIGUREDSQLCONNECTION_SECRET_NAME_OF_CONNECTION", true);
+            var connectionString = new SecretManager().GetSecretValue(secretValue, "connectionString");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        return optionsBuilder;
+    }
+}
