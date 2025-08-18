ae3a603719a05e149b48c9c12d7c17972146c979
ArdenHide <ardenhide@gmail.com>
2025-08-18 13:24:53 +0300

    - update `ConfiguredSqlConnection.Abstractions.Tests`

--- tests/ConfiguredSqlConnection.Abstractions.Tests/Extensions/ConnectionStringFromEnvironmentFactoryTests.cs
+++ tests/ConfiguredSqlConnection.Abstractions.Tests/Extensions/ConnectionStringFromEnvironmentFactoryTests.cs
@@ -2,27 +2,26 @@
 using FluentAssertions;
 using ConfiguredSqlConnection.Abstractions.Extensions;
 
-namespace ConfiguredSqlConnection.Abstractions.Tests.Extensions
+namespace ConfiguredSqlConnection.Abstractions.Tests.Extensions;
+
+public class ConnectionStringFromEnvironmentFactoryTests
 {
-    public class ConnectionStringFromEnvironmentFactoryTests
+    [Fact]
+    public void GetConnectionFromEnvironment_ReturnsConnectionString()
     {
-        [Fact]
-        public void GetConnectionFromEnvironment_ReturnsConnectionString()
+        var expected = "Server=.;Database=EnvDb;";
+        Environment.SetEnvironmentVariable("CONFIGUREDSQLCONNECTION_DB_MODE", ContextOption.Staging.ToString());
+        Environment.SetEnvironmentVariable("CONFIGUREDSQLCONNECTION_DB_NAME", "EnvDb");
+        var path = Path.Combine(Environment.CurrentDirectory, "appsettings.json");
+        File.WriteAllText(path, $"{{\"ConnectionStrings\":{{\"EnvDb\":\"{expected}\"}}}}");
+        try
         {
-            var expected = "Server=.;Database=EnvDb;";
-            Environment.SetEnvironmentVariable("CONFIGUREDSQLCONNECTION_DB_MODE", ContextOption.Staging.ToString());
-            Environment.SetEnvironmentVariable("CONFIGUREDSQLCONNECTION_DB_NAME", "EnvDb");
-            var path = Path.Combine(Environment.CurrentDirectory, "appsettings.json");
-            File.WriteAllText(path, $"{{\"ConnectionStrings\":{{\"EnvDb\":\"{expected}\"}}}}");
-            try
-            {
-                var actual = ConnectionStringFromEnvironmentFactory.GetConnectionFromEnvironment();
-                actual.Should().Be(expected);
-            }
-            finally
-            {
-                File.Delete(path);
-            }
+            var actual = ConnectionStringFromEnvironmentFactory.GetConnectionFromEnvironment();
+            actual.Should().Be(expected);
+        }
+        finally
+        {
+            File.Delete(path);
         }
     }
 }
---
+++ tests/ConfiguredSqlConnection.Abstractions.Tests/Extensions/DbContextEnvironmentFactoryTests.cs
@@ -0,0 +1,68 @@
+﻿using Moq;
+using Xunit;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using ConfiguredSqlConnection.Abstractions.Extensions;
+
+namespace ConfiguredSqlConnection.Abstractions.Tests.Extensions;
+
+public class DbContextEnvironmentFactoryTests
+{
+    private readonly ContextOption _contextOption = ContextOption.Prod;
+    private readonly DbContextOptionsBuilderFactory<DbContext> _optionsBuilderFactory;
+
+    public DbContextEnvironmentFactoryTests()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DbContext>().UseInMemoryDatabase("dbName");
+        var mockOptionsBuilderFactory = new Mock<DbContextOptionsBuilderFactory<DbContext>>();
+        mockOptionsBuilderFactory.Setup(x => x.Create(_contextOption, null)).Returns(optionsBuilder);
+        _optionsBuilderFactory = mockOptionsBuilderFactory.Object;
+    }
+
+    [Fact]
+    public void CreateFromEnvironment_ReturnsContext()
+    {
+        UseEnvironment(_contextOption.ToString(), string.Empty);
+        var factory = new Mock<DbContextEnvironmentFactory<DbContext>>(_optionsBuilderFactory);
+        factory.Setup(x => x.Create(_contextOption, null)).CallBase();
+        factory.Setup(x => x.CreateFromEnvironment()).CallBase();
+
+        var result = factory.Object.CreateFromEnvironment();
+
+        result.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void CreateFromEnvironment_OptionNotSet_Throws()
+    {
+        UseEnvironment(string.Empty, string.Empty);
+        var factory = new Mock<DbContextEnvironmentFactory<DbContext>>(_optionsBuilderFactory);
+        factory.Setup(x => x.Create(_contextOption, null)).CallBase();
+        factory.Setup(x => x.CreateFromEnvironment()).CallBase();
+
+        var act = factory.Object.CreateFromEnvironment;
+
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("Environment variable 'CONFIGUREDSQLCONNECTION_DB_MODE' is null or empty. (Parameter 'envValue')");
+    }
+
+    [Fact]
+    public void CreateFromEnvironment_InvalidOption_Throws()
+    {
+        UseEnvironment("Production", string.Empty);
+        var factory = new Mock<DbContextEnvironmentFactory<DbContext>>(_optionsBuilderFactory);
+        factory.Setup(x => x.Create(_contextOption, null)).CallBase();
+        factory.Setup(x => x.CreateFromEnvironment()).CallBase();
+
+        var act = factory.Object.CreateFromEnvironment;
+
+        act.Should().Throw<InvalidCastException>()
+            .WithMessage($"Failed to convert environment variable 'CONFIGUREDSQLCONNECTION_DB_MODE' to type '{typeof(ContextOption).FullName}'.");
+    }
+
+    private static void UseEnvironment(string? mode, string? name)
+    {
+        Environment.SetEnvironmentVariable("CONFIGUREDSQLCONNECTION_DB_MODE", mode);
+        Environment.SetEnvironmentVariable("CONFIGUREDSQLCONNECTION_DB_NAME", name);
+    }
+}
